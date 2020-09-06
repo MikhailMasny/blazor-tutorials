fa76baa350b1d56296a26422b288029dadae29e5
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-09-06 18:45:27 +0300

    feat: JavaScript interop example

---
+++ src/Masny.Blazor.ServerSide/wwwroot/js/interop.js
@@ -0,0 +1,22 @@
+﻿function useAlert() {
+    alert("Yes, you run it");
+}
+
+function usePromt(querstion) {
+    return prompt(querstion);
+}
+
+function setElementTextById(id, text) {
+    document.getElementById(id).innerText = text;
+}
+
+function focusOnElement(element) {
+    element.focus();
+}
+
+function giveMeRandomInt(maxValue) {
+    DotNet.invokeMethodAsync('Masny.Blazor.ServerSide', 'GenerateRandomInt', maxValue)
+        .then(result => {
+            setElementTextById('randomNumber', result);
+        });
+}
