54499b2ab64cdeb031405d8d3022def032b9ace8
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-09-06 17:05:10 +0300

    feat: add razor components

---
+++ src/Masny.Blazor.ServerSide/Code/ChildComponentBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Masny.Blazor.ServerSide.Code
+{
+    public class ChildComponentBase : ComponentBase
+    {
+        protected bool IsDarkTheme;
+        protected string AlertTheme => IsDarkTheme ? "dark" : "light";
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            IsDarkTheme = true;
+        }
+    }
+}
