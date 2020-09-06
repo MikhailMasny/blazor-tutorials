e2a995b182a3fbdcef16460d71af64b155acf383
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-09-06 20:45:58 +0300

    feat: form with validation

---
+++ src/Masny.Blazor.ServerSide/Data/JobApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Masny.Blazor.ServerSide.Data
+{
+    public class JobApplication
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [StringLength(500, ErrorMessage ="Desc error")]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(1000, 1500, ErrorMessage ="Salary error")]
+        public int Salary { get; set; }
+
+        [Required]
+        public bool IsOpenSource { get; set; }
+
+        [Required]
+        public DateTime Availability { get; set; }
+    }
+}
