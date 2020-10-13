269c255365e17307f864655b84addc2257f4024e
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-10-13 18:16:08 +0300

    feat: added implementation of form validation

---
+++ src/Masny.Blazor.ClientSide/Code/FormCode.cs
@@ -0,0 +1,44 @@
+﻿using Masny.Blazor.ClientSide.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Masny.Blazor.ClientSide.Code
+{
+    public class FormCode : ComponentBase
+    {
+        protected FormModel model = new FormModel();
+        protected EditContext editContext;
+
+        protected List<string> InputSelectList { get; set; } = new List<string>
+        {
+            string.Empty,
+            "Mr",
+            "Mrs",
+            "Miss",
+            "Ms"
+        };
+
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+
+        protected override void OnInitialized()
+        {
+            editContext = new EditContext(model);
+        }
+
+        protected void HandleValidSubmit()
+        {
+            var modelJson = JsonSerializer.Serialize(model, new JsonSerializerOptions { WriteIndented = true });
+            JSRuntime.InvokeVoidAsync("alert", $"Your data:\n\n{modelJson}");
+        }
+
+        protected void HandleReset()
+        {
+            model = new FormModel();
+            editContext = new EditContext(model);
+        }
+    }
+}
---
+++ src/Masny.Blazor.ClientSide/Models/FormModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Masny.Blazor.ClientSide.Models
+{
+    public class FormModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Birth")]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string Password { get; set; }
+
+        [Required]
+        //[CompareProperty("Password")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Accept Ts & Cs is required")]
+        public bool AcceptTerms { get; set; }
+    }
+}
