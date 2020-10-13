7386c7c9b9fc589cb872659b0aeb0b2284b8ae44
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-10-13 18:03:59 +0300

    feat: implemented GET and POST requests by HttpClient

---
+++ src/Masny.Blazor.ClientSide/Code/GetRequestCode.cs
@@ -0,0 +1,32 @@
+﻿using Masny.Blazor.ClientSide.Models;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Masny.Blazor.ClientSide.Code
+{
+    //[Authorize(Roles = "Role")]
+    public class GetRequestCode : ComponentBase
+    {
+        protected IEnumerable<UserResponse> Response;
+        protected string ResponseErrorMessage;
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            using var httpResponse = await HttpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                ResponseErrorMessage = httpResponse.ReasonPhrase;
+                return;
+            }
+
+            Response = await httpResponse.Content.ReadFromJsonAsync<IEnumerable<UserResponse>>();
+        }
+    }
+}
---
+++ src/Masny.Blazor.ClientSide/Code/PostRequestCode.cs
@@ -0,0 +1,42 @@
+﻿using Masny.Blazor.ClientSide.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Masny.Blazor.ClientSide.Code
+{
+    //[Authorize(Roles = "Role")]
+    public class PostRequestCode : ComponentBase
+    {
+        protected PostResponse Response;
+        protected string ResponseErrorMessage;
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://jsonplaceholder.typicode.com/posts");
+            var postBody = new
+            {
+                title = "foo",
+                body = "bar",
+                userId = 1,
+            };
+            request.Content = new StringContent(JsonSerializer.Serialize(postBody), Encoding.UTF8, "application/json");
+
+            using var httpResponse = await HttpClient.SendAsync(request);
+
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                ResponseErrorMessage = httpResponse.ReasonPhrase;
+                return;
+            }
+
+            Response = await httpResponse.Content.ReadFromJsonAsync<PostResponse>();
+        }
+    }
+}
